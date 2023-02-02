db2f970585d4fc545e58b6d5b4167b4e35b72ee3
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:42:13 +0530

    commit

---
+++ src/trim.java
@@ -0,0 +1,7 @@
+public class trim {
+    public static void main(String args[]) {
+        String s1 = "  hello Srinivas   ";
+        System.out.println(s1 + " nice to meet you");
+        System.out.println(s1.trim() + " nice to meet you");
+    }
+}
