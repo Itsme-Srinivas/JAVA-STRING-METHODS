1d5ef95cf8fec98f2d409ffb03690f97cb263248
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 22:48:24 +0530

    commit

---
+++ src/contains.java
@@ -0,0 +1,10 @@
+public class contains {
+    public static void main(String[] args) {
+
+        String s = "Hey Papa, Here Mr_Srinivas Daa";
+
+        System.out.println(s.contains("Hey Papa"));
+        System.out.println(s.contains("Hello"));
+        System.out.println(s.contains("Mr_Srinivas"));
+    }
+}
