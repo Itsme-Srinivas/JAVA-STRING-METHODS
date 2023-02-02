8aaa39012216ea248af13f212328c7a6ae97210f
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:39:57 +0530

    commit

---
+++ src/TOuPPERcASE.java
@@ -0,0 +1,7 @@
+public class TOuPPERcASE {
+    public static void main(String args[]) {
+        String s1 = "Your are correct Daa !";
+        String s1upper = s1.toUpperCase();
+        System.out.println(s1upper);
+    }
+}
