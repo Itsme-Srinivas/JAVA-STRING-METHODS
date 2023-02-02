79c87536689f65787cd7fdb570f74caa39f54ebd
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:20:09 +0530

    commit

---
+++ src/length.java
@@ -0,0 +1,8 @@
+public class length {
+    public static void main(String args[]) {
+        String s1 = "Srinivas";
+        String s2 = "Pannir";
+        System.out.println("string length is: " + s1.length());
+        System.out.println("string length is: " + s2.length());
+    }
+}
