ca86576484d8a43c0e750db34d3a307567d4235c
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:18:17 +0530

    commit

---
+++ src/lastindexOf.java
@@ -0,0 +1,7 @@
+public class lastindexOf {
+    public static void main(String args[]) {
+        String s1 = "Hey Pannir Love u Daa";
+        int index1 = s1.lastIndexOf('P');
+        System.out.println(index1);
+    }
+}
