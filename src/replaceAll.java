b64f36beb9d03890e7e27ff2f8e274101491845a
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:24:47 +0530

    commit

---
+++ src/replaceAll.java
@@ -0,0 +1,7 @@
+public class replaceAll {
+    public static void main(String args[]) {
+        String s1 = "Herz, Mr Srinivas";
+        String replaceString = s1.replaceAll("z", "e");
+        System.out.println(replaceString);
+    }
+}
