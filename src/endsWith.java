02219e61edb68963b9975f8e46da7d44d1edd9f8
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 22:52:56 +0530

    commit

---
+++ src/endsWith.java
@@ -0,0 +1,10 @@
+public class endsWith {
+    public static void main(String[] args) {
+
+        String s = "Hello Srinivas";
+
+        System.out.println(s.endsWith("s"));
+        System.out.println(s.endsWith("a"));
+        System.out.println(s.endsWith("Srinivas"));
+    }
+}
---
+++ src/equals.java
@@ -0,0 +1,13 @@
+public class equals {
+    public static void main(String[] args) {
+
+        String s1 = "Srinivas";
+        String s2 = "Tom";
+        String s3 = "Cat";
+        String s4 = "Wolf";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equals(s4));
+    }
+}
