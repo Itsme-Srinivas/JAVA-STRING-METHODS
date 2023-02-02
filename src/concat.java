72542c7f6a91eda3a56a51962a4d98c8717b22f7
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 22:44:24 +0530

    commit

---
+++ src/concat.java
@@ -0,0 +1,10 @@
+public class concat {
+    public static void main(String[] args) {
+
+        String s1 = "Srinivas";
+        s1.concat(" How are you man !");
+        System.out.println(s1);
+        s1 = s1.concat(" How are you man !");
+        System.out.println(s1);
+    }
+}
