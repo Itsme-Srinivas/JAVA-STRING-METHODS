4b18484f20ebf55905c652b0a74d52bf5dc7a081
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 22:40:04 +0530

    commit

---
+++ src/compareTo.java
@@ -0,0 +1,12 @@
+public class compareTo {
+    public static void main(String[] args) {
+
+        String s1 = "Srinivas";
+        String s2 = "Pannir";
+        String s3 = "Potti";
+
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s3.compareTo(s1));
+    }
+}
