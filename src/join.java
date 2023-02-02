b2b2775b745993cafb85f9dd59076885b5919ec2
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:16:24 +0530

    commit

---
+++ src/join.java
@@ -0,0 +1,6 @@
+public class join {
+    public static void main(String args[]) {
+        String joinString1 = String.join("-", "welcome", "to", "My World Pannir");
+        System.out.println(joinString1);
+    }
+}
