ac189dde5b516c8e421444c9385aed9c4a140f83
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:23:09 +0530

    commit

---
+++ src/replace.java
@@ -0,0 +1,7 @@
+public class replace {
+    public static void main(String args[]) {
+        String s1 = "Panniz is my Wife";
+        String replaceString = s1.replace('z', 'r');
+        System.out.println(replaceString);
+    }
+}
