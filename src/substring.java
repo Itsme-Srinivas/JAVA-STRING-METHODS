91814fec03acc51f9c6be2c09e2caa4bc7a7c790
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:30:11 +0530

    commit

---
+++ src/substring.java
@@ -0,0 +1,7 @@
+public class substring {
+    public static void main(String args[]) {
+        String s1 = "Srinivas";
+        System.out.println(s1.substring(2, 4));
+        System.out.println(s1.substring(2));
+    }
+}
