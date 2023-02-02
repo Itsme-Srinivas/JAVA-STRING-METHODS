6338f57336c91e585bf9ae0a5bf9db2fad1d4cbe
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:14:40 +0530

    commit

---
+++ src/isEmpty.java
@@ -0,0 +1,9 @@
+public class isEmpty {
+    public static void main(String args[]) {
+        String s1 = "";
+        String s2 = "Hello Daa";
+
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isEmpty());
+    }
+}
