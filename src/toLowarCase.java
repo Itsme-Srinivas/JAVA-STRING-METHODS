2e9ea317c932973e64062f3f96eea8d7243964c4
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:33:47 +0530

    commit

---
+++ src/toLowarCase.java
@@ -0,0 +1,7 @@
+public class toLowarCase {
+    public static void main(String args[]) {
+        String s1 = "HEY I THINK YOUR ARE IN LOVE WITH YOUR WIFE";
+        String s1lower = s1.toLowerCase();
+        System.out.println(s1lower);
+    }
+}
