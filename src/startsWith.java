b2f988ca9c02a7dc64c2aaa7d6862694d3f09394
Itsme-Srinivas <srinivasarao.t.1947@gmail.com>
2023-02-02 23:28:50 +0530

    commit

---
+++ src/startsWith.java
@@ -0,0 +1,9 @@
+public class startsWith {
+    public static void main(String args[])
+    {
+        String s1="Rey Srinivas, Where is Pannir Daa";
+        System.out.println(s1.startsWith("Rey"));
+        System.out.println(s1.startsWith("Pannir"));
+        System.out.println(s1.startsWith("Daa"));
+    }
+}
